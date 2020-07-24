59125b361fdd7ffdbab430b4ad74fafcdb574435
Pashmentov Dmitry <dvpashmentov@gmail.com>
2020-07-24 22:24:22 +0300

    Part 7: Forms.

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from rango.forms import CategoryForm
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 from rango.models import Category, Page
 
@@ -27,3 +29,18 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     
     return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == "POST": 
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+
+            return index(request)
+        else: 
+            print(form.errors)
+        
+    return render(request, 'rango/add_category.html', {'form': form})
