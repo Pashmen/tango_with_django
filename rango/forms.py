d3132d7f38855e3c4a70049e5975759b2b9f3d1c
Pashmentov Dmitry <dvpashmentov@gmail.com>
2020-07-29 15:35:13 +0300

    Part 9: User Authentication.

--- rango/forms.py
+++ rango/forms.py
@@ -1,17 +1,18 @@
 from django import forms
-from rango.models import Category, Page
+from django.contrib.auth.models import User
+
+from rango.models import Category, Page, UserProfile
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
-
-    # An inline class to provide additional information on the form.
+    
     class Meta:
-        # Provide an association between the ModelForm and a model
         model = Category
         fields = ('name',)
+
 
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128,
@@ -33,3 +34,17 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         exclude = ('category',)
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
