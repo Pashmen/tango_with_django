ffd0b76ad590ce709261057e180f8e66c719e3ac
Pashmentov Dmitry <dvpashmentov@gmail.com>
2020-07-30 16:51:03 +0300

    Part 9: User Authentication.

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,8 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
-
-
 
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from rango.models import Category, Page
@@ -120,3 +118,14 @@ def user_login(request):
             return HttpResponse("Invalid login details supplied.")
     else:
         return render(request, 'rango/login.html', {})
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    
+    return HttpResponseRedirect(reverse('index'))
