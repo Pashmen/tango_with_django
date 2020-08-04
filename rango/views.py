52359fbedca6d115a40f27166467683d7c78fb35
Pashmentov Dmitry <dvpashmentov@gmail.com>
2020-08-04 16:17:08 +0300

    Part 10: Cookies and sessions.

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
@@ -11,10 +13,14 @@ from rango.models import Category, Page
 def about(request):
     return render(request, 'rango/about.html')
 
-def index(request):    
+def index(request):        
+    visitor_cookie_handler(request)
+
     category_list = Category.objects.order_by('-likes')[:5]
-    page_list = Page.objects.order_by('-views')[:5]
-    context_dict = {'categories': category_list, 'pages': page_list}
+    page_list = Page.objects.order_by('-views')[:5]        
+    visits = request.session['visits']
+
+    context_dict = {'categories': category_list, 'pages': page_list, 'visits': visits}       
 
     return render(request, 'rango/index.html', context=context_dict)
 
@@ -131,3 +137,22 @@ def user_logout(request):
     logout(request)
     
     return HttpResponseRedirect(reverse('index'))
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+
+    return val
+
+def visitor_cookie_handler(request):        
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1        
+        last_visit_cookie = str(datetime.now())
+
+    request.session['last_visit'] = last_visit_cookie
+    request.session['visits'] = visits
