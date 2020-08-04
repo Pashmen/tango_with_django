9f6d7fef007a342ac812b1ad780d832ce35d83eb
Pashmentov Dmitry <dvpashmentov@gmail.com>
2020-08-04 11:44:07 +0300

    Part 9: User Authentication (Exercises).

--- rango/views.py
+++ rango/views.py
@@ -32,6 +32,7 @@ def show_category(request, category_name_slug):
     
     return render(request, 'rango/category.html', context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -47,6 +48,7 @@ def add_category(request):
         
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -115,14 +117,14 @@ def user_login(request):
         else:
             print("Invalid login details: {0}, {1}".format(username, password))
 
-            return HttpResponse("Invalid login details supplied.")
+            return HttpResponse("Invalid login details supplied. <br> \n  \
+                <a href='.'> Back <a>")
     else:
         return render(request, 'rango/login.html', {})
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
-
+    return render(request, 'rango/restricted.html')
 
 @login_required
 def user_logout(request):
